adb095e1cb07a138bdc0c682f66cc58d740a92b4
semoon <semonec@gmail.com>
2020-12-31 19:22:30 +0700

    fix

--- pages/api/test.js
+++ pages/api/test.js
@@ -3,7 +3,6 @@ import { connectToDatabase } from "../../util/mongodb";
 export default async (req, res) => {
   const { db } = await connectToDatabase();
   console.log(req);
-  console.log(JSON.stringify(req));
 
   const movies = await db
     .collection("sabai")
