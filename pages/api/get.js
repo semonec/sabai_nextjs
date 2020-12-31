042490cceb89169efdea4f64500ca5cc9069795a
semoon <semonec@gmail.com>
2021-01-01 00:31:30 +0700

    get post added

--- pages/api/get.js
+++ pages/api/get.js
@@ -3,13 +3,10 @@ export default async (req, res) => {
   const { db } = await connectToDatabase();
   console.log(req);
 
-  const item = {
-    time: new Date.now(), 
-    data: JSON.stringify(req.body)
-  };
-
   const movies = await db
     .collection("sabai")
-    .insertOne(item)
-  res.json(item);
+    .find({})
+    .limit(20)
+    .toArray();
+  res.json(movies);
 };
